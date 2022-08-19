80466647bc56cd1186623ed7e6fb2c555ec13271
Andrey Udot <andrey.u@lovisnami.ru>
2022-08-19 10:12:29 +0300

    Udot массивы

--- public/lesson1Udot.php
+++ public/lesson1Udot.php
@@ -1,6 +1,9 @@
 <?php
 $name = 'andrey';
 $workExperince = 10;
-echo($name);
-echo("<br>");
-echo($workExperince);
+//echo($name);
+var_dump($name);
+var_dump('<br>' );
+var_dump($workExperince );
+//echo("<br>");
+//echo($workExperince);
---
+++ public/lesson2Udot.php
@@ -0,0 +1,24 @@
+<?php
+$a = [];
+$b = [2,4,6];
+$c = [[3,5,3], [2,6,7]];
+//echo("<pre>");
+//echo($c);
+print_r ($c);
+//echo ($c[0][1]); //вывод 5
+//echo ($c[1][1]);
+//echo('<br>');
+//echo ($c[1][2]);
+//echo ('<br>');
+$array = $c[1][2]+$c[1][2];
+//echo($array);
+$array1 = $c[0][2]+$c[1][1];
+//echo ('<br>');
+//echo ($array1)
+$array2 =[];
+$array2 = [[1,2,300],[1,2,400],[2,6,7]];
+echo('<pre>');
+print_r($array2);
+$array2[0] =[];
+
+
