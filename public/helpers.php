2437cdd319a473de147627aac2716803f3a60001
Ivan Shepelev <yonishepelev@gmail.com>
2022-09-02 14:36:23 +0300

    добавил новую фукнцию

--- public/helpers.php
+++ public/helpers.php
@@ -7,6 +7,12 @@ function print_rr($var)
     echo("\n</pre>");
 
 }
+function print_re($var) {
+    echo("\n<pre>\n");
+    print_r($var);
+    echo("\n</pre>");
+    die();
+}
 
 function divideNum(float $firstNumber, float $secondNumber): float
 {
