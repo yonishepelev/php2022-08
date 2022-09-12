dc6fbaf75494632055595cbb8a3a5425aa4ef719
shplet <shplet1@mail.ru>
2022-09-12 21:53:58 +0300

    lesson12 shplet

--- public/App/Models/PhoneShplet.php
+++ public/App/Models/PhoneShplet.php
@@ -4,20 +4,41 @@ namespace App\Models;
 
 class PhoneShplet extends Phone
 {
+    static private int $box = 5;
     protected int $length;
     protected int $width;
     protected int $height;
 
     /**
-     * @param int $length  длинна в мм
-     * @param int $width   ширина в мм
-     * @param int $height  высота в мм
+     * @param int $length длинна в мм
+     * @param int $width ширина в мм
+     * @param int $height высота в мм
      * @return void
      */
     public function setDimensions(int $length, int $width, int $height)
-{
-    $this->length = $length;
-    $this->width = $width;
-    $this->weight = $width;
-}
+    {
+        $this->length = $length;
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function getBox(): int
+    {
+        return self::$box;
+    }
+
+    public function setBox(int $number): void
+    {
+        self::$box = $number;
+    }
+
+    public function getvolume()
+    {
+        $volume = $this->length * $this->height * $this->width;
+        return $volume;
+    }
+    public static function getPlace():string
+    {
+        return 'Рыбацкий 10';
+    }
 }
--- public/lessons/lesson10Shplet.php
+++ public/lessons/lesson10Shplet.php
@@ -1,2 +1,22 @@
 <?php
+
+use App\Models\PhoneShplet;
+
 require_once '../include/common_function.php';
+
+$phoneShplet = new PhoneShplet('Xiomi', 350, 'blue', 150);
+$phoneShplet2 = new PhoneShplet('Xiomi11', 350, 'blue', 150);
+$phoneShplet->setDimensions(150, 25, 200);
+echo '<pre>';
+var_dump($phoneShplet);
+var_dump($phoneShplet2);
+var_dump($phoneShplet->getBox());
+var_dump($phoneShplet2->getBox());
+
+//print_rr($phoneShplet);
+//echo $phoneShplet->getvolume() . ' mm';
+$phoneShplet->setBox(10);
+var_dump($phoneShplet->getBox());
+var_dump($phoneShplet2->getBox());
+
+print_rr($phoneShplet);
