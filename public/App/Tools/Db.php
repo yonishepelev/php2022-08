8ddb314f9446a5bb0dc64a8fc28158092b402a8e
Ivan Shepelev <yonishepelev@gmail.com>
2022-10-06 09:13:31 +0200

    урок 12

---
+++ public/App/Tools/Db.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Tools;
+
+use PDO;
+
+class Db
+{
+    private static self $instance;
+    private $connection;
+
+
+    private function __construct()
+    {
+        $hostname = 'db';
+        $username = 'root';
+        $password = 'password';
+        $database = 'our_shop';
+        $port = 3306;
+        $conn = new PDO( "mysql:host=$hostname;dbname=$database;port=$port", $username, $password );
+        $conn->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        $this->connection = $conn;
+
+
+    }
+
+    private function __clone()
+    {
+    }
+
+    public static function connect()
+    {
+        if (isset( self::$instance ))
+            return self::$instance;
+
+        self::$instance = new self();
+        return self::$instance;
+    }
+
+    public function query($sql)
+    {
+        return $this->connection->query ($sql)->fetchAll (PDO::FETCH_CLASS);
+    }
+
+}
